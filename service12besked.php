e7b132f80d9caa6445696cff9b979ef492aea152
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2021-05-06 20:44:25 +0200

    formen virker stadig ikke

--- service12besked.php
+++ service12besked.php
@@ -1,13 +1,12 @@
 <?php
-    // $email=$_REQUEST['email'];
-    // $navn=$_REQUEST['navn'];
-    // $telnr=$_REQUEST['telnr'];
-    // $emne=$_REQUEST['emne'];
-    // $besked="<p style='font-family: sans-serif; font-size:18px; color:black'>".$_REQUEST['besked']."</p>";
+    $email=$_REQUEST['email'];
+    $navn=$_REQUEST['navn'];
+    $telnr=$_REQUEST['telnr'];
+    $emne=$_REQUEST['emne'];
+    $besked="<p style='font-family: sans-serif; font-size:18px; color:black'>".$_REQUEST['besked']."</p>";
 
-    // $til="lene_frkjr@yahoo.dk";
+    $til="lene_frkjr@yahoo.dk";
 
-    // mail($til, "besked fra $navn: $emne", $besked, "Content-type:text/html; charset=utf-8\r\nfrom: $email");
-    // mail("lene_frkjr@yahoo.dk","emne","tekst");
-    // header("location: service12tak.html");
+    mail($til, "besked fra $navn: $emne", $besked, "Content-type:text/html; charset=utf-8\r\nfrom: $email");
+    header("location: service12tak.html");
 ?>
