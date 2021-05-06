1a964b03d9686edb857557338ed68d2f9fdad7df
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2021-05-06 15:41:51 +0200

    formen virker ikke

---
+++ service12besked.php
@@ -0,0 +1,13 @@
+<?php
+    // $email=$_REQUEST['email'];
+    // $navn=$_REQUEST['navn'];
+    // $telnr=$_REQUEST['telnr'];
+    // $emne=$_REQUEST['emne'];
+    // $besked="<p style='font-family: sans-serif; font-size:18px; color:black'>".$_REQUEST['besked']."</p>";
+
+    // $til="lene_frkjr@yahoo.dk";
+
+    // mail($til, "besked fra $navn: $emne", $besked, "Content-type:text/html; charset=utf-8\r\nfrom: $email");
+    // mail("lene_frkjr@yahoo.dk","emne","tekst");
+    // header("location: service12tak.html");
+?>
