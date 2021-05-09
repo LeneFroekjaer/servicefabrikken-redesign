1cc16ec0f3664ac9bc1b9766ea6175bb211fedbf
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2021-05-09 16:58:49 +0200

    er igang med at validere formen

---
+++ js/kontakt.js
@@ -0,0 +1,14 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", init);
+const form = document.querySelector("form")
+const email = form.elements.f_email
+const navn = form.elements.f_navn
+
+
+function init(){
+
+}
+
+function validateForm(){
+
+}
