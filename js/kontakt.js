571230e485cad8d9cb1b014a7fc01f2b17181204
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2021-05-10 10:25:03 +0200

    lavet formularen paa kontaktsiden

--- js/kontakt.js
+++ js/kontakt.js
@@ -3,12 +3,37 @@ window.addEventListener("DOMContentLoaded", init);
 const form = document.querySelector("form")
 const email = form.elements.f_email
 const navn = form.elements.f_navn
+const emailError = document.querySelector("#f_email + span.error")
+const navnError = document.querySelector("#f_navn + span.error")
 
 
 function init(){
-
+    email.addEventListener("input", validateEmail);
+    navn.addEventListener("input", validateNavn);
 }
 
-function validateForm(){
+function validateEmail(){
+    email.classList.add("check");
+    if (email.validity.valid){
+        emailError.textContent = "";
+        emailError.className = "error";
+        email.style.borderRadius = "50px"
+    } else {
+        emailError.textContent = "Indsæt venligst en gyldig email";
+        emailError.className = "error active";
+        email.style.borderRadius = "25px 25px 0 0"
+    }
+}
 
+function validateNavn(){
+    navn.classList.add("check");
+    if (navn.validity.valid){
+        navnError.textContent = "";
+        navnError.className = "error";
+        navn.style.borderRadius = "50px"
+    } else {
+        navnError.textContent = "Indsæt venligst dit navn";
+        navnError.className = "error active";
+        navn.style.borderRadius = "25px 25px 0 0"
+    }
 }
