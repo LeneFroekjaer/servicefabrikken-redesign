120cba643a22e85315e6f02ea4eea24f847305bf
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2021-05-16 21:28:03 +0200

    slettet fra

--- service12besked.php
+++ service12besked.php
@@ -3,7 +3,7 @@
     $navn=$_REQUEST['navn'];
     $telnr=$_REQUEST['telnr'];
     $emne=$_REQUEST['emne'];
-    $besked=nl2br("<p style='font-family: sans-serif; font-size:18px; color:black'>"."fra: ".$_REQUEST['besked']."\r\n"."fra: "."\r\n".$navn."\r\n".$telnr."\r\n".$email."</p>");
+    $besked=nl2br("<p style='font-family: sans-serif; font-size:18px; color:black'>".$_REQUEST['besked']."\r\n"."fra: "."\r\n".$navn."\r\n".$telnr."\r\n".$email."</p>");
 
     $til="lene_frkjr@yahoo.dk";
     $fra="lenefroekjaer@lenefroekjaer.dk";
